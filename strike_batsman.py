4db421e8dcf3e100d7c15bc5f0d20dbe3065f6fa
vivekatharva45 <vivekatharva171@gmail.com>
2026-07-26 16:52:46 +0530

    Enhance strike rate output with performance comments
    Added performance evaluation based on strike rate.

--- strike_batsman.py
+++ strike_batsman.py
@@ -6,21 +6,31 @@ def main():
 
     rate = Runs/Balls * 100
 
-    print(f"The strike rate is {rate:.2f}")
+    print(f"{Name}'s strike rate was {rate:.2f}")
+
+
+
+    if rate <=130:
+     print("This was a slow innings.")
+
+
+    if 130<rate<=150:
+       print("This was a decent innings.")
+
+    if 150<rate<=180:
+     print("This was a good innings.")
+
+    if 180<rate:
+       print("This was an outstanding innings!")
 
 
 def Batsman_runs(B):
     return int(B)
 
 
-
-
-
-
 def Balls_faced(G):
     return int(G)
 
 main()
-
 
 
