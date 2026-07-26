f3ced8419043e25c65975d118f41cd4341af42d2
vivekatharva45 <vivekatharva171@gmail.com>
2026-07-27 01:41:42 +0530

    Version 2.0
    New features added:
        Menu display
        Economy calculation
        Average calculation

---
+++ strike_batsman (1).py
@@ -0,0 +1,103 @@
+print("Welcome to Atharva's Cricket Stat Toolkit" )
+print("3 Features available:")
+print("1.Batting Strike Rate")
+print("2.Bowling economy")
+print("3.Batting average")
+
+X = input("Please type the number to choose the option:")
+
+def strike():
+    Name = input("What is the name of the batsman? ")
+    Runs = Batsman_runs(input("How many runs did the batsman score? "))
+    Balls = Balls_faced(input("How many balls were faced by the batsman? "))
+
+    rate = Runs/Balls * 100
+
+    print(f"{Name}'s strike rate was {rate:.2f}")
+
+
+
+    if rate <=130:
+     print("This was a slow innings.")
+
+
+    if 130<rate<=150:
+       print("This was a decent innings.")
+
+    if 150<rate<=180:
+     print("This was a good innings.")
+
+    if 180<rate:
+       print("This was an outstanding innings!")
+
+
+def Batsman_runs(B):
+    return int(B)
+
+
+def Balls_faced(G):
+    return int(G)
+
+
+def economy():
+   Names = input("Please specify the name of the bowler? ")
+   Conc = Runsconceded(input("How many runs did the bowler concede? "))
+   Over = overbowled(input("How many overs did the bowler bowl? "))
+
+
+   eco = Conc/Over
+
+   print(f"{Names}'s economy is {eco:.2f}")
+
+   if eco<6:
+      print("The bowler was extremely economical.")
+
+   if 6<=eco<9:
+      print("The bowler was good.")
+
+   if eco>9:
+      print("The bowler was expensive.")
+
+
+def Runsconceded(a):
+   return int(a)
+
+def overbowled(O):
+   return float(O)
+
+
+def average():
+   Namess = input("Specify the name of the batsman: ")
+   Runs_scored=scoredruns(input("How many runs did the batsman score?: "))
+   Timesout=innings(input("How many times did the batsman get out?: "))
+
+   avg = Runs_scored/Timesout
+
+   print(f"{Namess}'s average is {avg:.2f}")
+
+def scoredruns(x):
+      return int(x)
+
+def innings(i):
+      return int(i)
+
+if X=="1":
+   strike()
+
+
+elif X =="2":
+   economy()
+
+
+elif X == "3":
+   average()
+
+else:
+   print("invalid")
+
+
+
+
+
+
+
